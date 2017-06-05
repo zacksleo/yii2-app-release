8d13a96d2012fd74505f4e42ad7881825f151652
zacksleo <zacksleo@gmail.com>
2017-06-05 22:43:43 +0800

    add zh-TW

---
+++ src/messages/zh-TW/apprelease.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'app-release' => 'App發布',
+    'App Releases' => 'App發布',
+    'Create' => '創建',
+    'published' => '已發布',
+    'unpublished' => '未發布',
+    'Yes' => '是',
+    'No' => '否',
+    'version' => '版本號',
+    'is forced' => '強制更新',
+    'url' => '下載地址',
+    'status' => '狀態',
+    'description' => '發布說明',
+    'created at' => '創建時間',
+    'updated at' => '更新時間',
+    'File' => '文件',
+];
